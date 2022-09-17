b8c6b5c2b34b7f99493f1165e5a7221fe46bda70
LaLaGuy <26328210+LaLaGuy@users.noreply.github.com>
2022-09-17 13:28:26 +0200

    fixes

--- Containerfile
+++ Containerfile
@@ -1,5 +1,5 @@
 FROM quay.io/centos/centos:stream8
-RUN yum update -y && yum install -y epel-release && yum install -y screen
+RUN yum update -y && yum install -y epel-release screen procps
 RUN curl https://docs.nexthink.com/integrations/840925474/nxeventconnector-1.4.0-1.el7.noarch.rpm -o ~/nxeventconnector-1.4.0-1.el7.noarch.rpm
 RUN screen -mdS "nxinstall" bash -c "yum install -y ~/nxeventconnector*";sleep 20s;screen -S "nxinstall" -X stuff "1";sleep 10s
 COPY ./stay_alive.sh /root/
--- stay_alive.sh
+++ stay_alive.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 /usr/bin/nxeventconnector $@
+trap "kill `ps -e  | grep nxeventconnector | awk '{print $1}'`; exit" SIGTERM
 unset nxeventconnector_alive
-nxeventconnector_alive=`ps -a | grep -F "nxeventconnector" | grep -v "grep -F "`
+nxeventconnector_alive=`ps -e | grep -F "nxeventconnector"`
 while [ -n "$nxeventconnector_alive" ]
 do
 sleep 5s
-nxeventconnector_alive=`ps -a | grep -F "nxeventconnector" | grep -v "grep -F "`
+nxeventconnector_alive=`ps -e | grep -F "nxeventconnector"`
 echo "$nxeventconnector_alive" >> /root/log.log
 done
 unset nxeventconnector_alive
